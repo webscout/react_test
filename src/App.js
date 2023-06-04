d73cc516b6e9091bde6f3b837bda599c5c0bd611
alexeyb <avbaranov@detmir.ru>
2023-06-04 14:36:30 +0300

    Add simple performance test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useRef, useState} from 'react';
+
+function LongRender() {
+  const rows = [];
+
+  for (let i = 0; i < 100; i++) {
+    const row = [];
+
+    for (let j = 0; j < 100; j++) {
+      const color = `rgb(${155+i}, ${55+j+i}, ${155+j})`;
+
+      row.push(
+        <div className="item" style={{backgroundColor: color }}>{i}<br/>{j}</div>
+      );
+    }
+
+    rows.push(
+      <div className="row">
+        {row}
+      </div>
+    );
+  }
+
+  return <>
+    {rows}
+  </>
+}
 
 function App() {
+  const renderStart = useRef();
+  const [renderTime, setRenderTime] = useState(null);
+
+  renderStart.current = Date.now();
+  useEffect(() => {
+    setRenderTime(Date.now() - renderStart.current);
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="renderTime">
+        <div>
+          {renderTime && renderTime}
+        </div>
+      </div>
+      <LongRender/>
     </div>
   );
 }
